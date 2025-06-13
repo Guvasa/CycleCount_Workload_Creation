ef7901e71626879bed877d83b2a23defe41cc8c9
Guvasa <30557607+Guvasa@users.noreply.github.com>
2025-06-13 13:01:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import io
 ##st.subheader("📤 Upload Cycle Count Excel File")
 ##uploaded_file = st.file_uploader("Please upload the 'CycleCount-DataGatering.xlsm' file", type=["xlsx"])
 
-st.subheader("📤 Upload Cycle Count Excel File")
+st.subheader("📤 Upload Data - Cycle Count Excel File")
 uploaded_file = st.file_uploader("Please upload the 'CycleCount-DataGatering.xlsx' file", type=["xlsm"])
 
 if uploaded_file is not None:
