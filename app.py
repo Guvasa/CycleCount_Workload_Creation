0a303e03df3d0e1654cf5fdb33f6c8002d9c6baa
Guvasa <30557607+Guvasa@users.noreply.github.com>
2025-06-23 11:07:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -109,8 +109,9 @@ upper = Q3 + 1.5 * IQR
 df["AVGPrice"] = np.where(df["AVGPrice"] < lower, lower, df["AVGPrice"])
 df["AVGPrice"] = np.where(df["AVGPrice"] > upper, upper, df["AVGPrice"])
 
-st.write("\u2003\u2003⚖ Normalizing data...")
-#st.markdown("&emsp;&emsp;⚖ Normalizing data...")
+#st.write("\u2003\u2003⚖ Normalizing data...")
+st.markdown("<span style='padding-left: 2em;'>⚖ Normalizing data...</span>", unsafe_allow_html=True)
+
 
 # Plot distributions AFTER normalization
 st.write("📊 Plotting distributions after normalization...")
